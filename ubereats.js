3d076cdc0303d755896bba6ec6bdc4f74b69eda5
Shobhit141141 <122107079+Shobhit141141@users.noreply.github.com>
2023-08-04 11:01:17 +0530

    ubereats.js

---
+++ ubereats.js
@@ -0,0 +1,96 @@
+var button=document.getElementById('but'); 
+var toggle=document.getElementById('ball'); 
+var tog=document.getElementById('toggle'); 
+var header=document.getElementById('header'); 
+var signup=document.getElementById('signup'); 
+var sign=document.getElementById('sign'); 
+var logo=document.getElementById('logo'); 
+var findfoo=document.getElementById('findfoo'); 
+var findfood=document.getElementById('findfood');
+button.onclick=function (){ 
+     
+     if(toggle.className=='open') 
+     { 
+         toggle.className=""; 
+  
+  
+     }else{ 
+         toggle.className="open"; 
+  
+     } 
+     
+     
+     if(tog.className=='open') 
+     { 
+         tog.className=""; 
+  
+  
+     }else{ 
+         tog.className="open"; 
+  
+     } 
+     
+     if(header.className=='open') 
+     { 
+         header.className=""; 
+  
+  
+     }else{ 
+         header.className="open"; 
+  
+     } 
+     
+     if(sign.className=='open') 
+     { 
+         sign.className=""; 
+  
+  
+     }else{ 
+         sign.className="open"; 
+  
+     } 
+     
+     if(signup.className=='open') 
+     { 
+         signup.className=""; 
+  
+  
+     }else{ 
+         signup.className="open"; 
+  
+     } 
+     
+     
+     if(logo.className=='open') 
+     { 
+         logo. className=""; 
+  
+  
+     }else{ 
+         logo.className="open"; 
+  
+     } 
+     
+     
+     if(findfoo.className=='open') 
+     { 
+         findfoo.className=""; 
+  
+  
+     }else{ 
+         findfoo.className="open"; 
+  
+     } 
+     
+     
+     if(findfood.className=='open') 
+     { 
+         findfood.className=""; 
+  
+  
+     }else{ 
+         findfood.className="open"; 
+  
+     } 
+  
+ };
