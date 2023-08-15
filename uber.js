d74f9753486fad6f0db7d344c61a418791ce7786
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2023-08-15 17:13:34 +0530

    Create uber.js

---
+++ uber.js
@@ -0,0 +1,161 @@
+var button=document.getElementById('but');  
+ var toggle=document.getElementById('ball');  
+ var tog=document.getElementById('toggle');  
+ var app=document.querySelector('#applink');  
+ var body=document.querySelector('body');
+ var header=document.querySelector('#header');
+ var btn=document.querySelector('#btn');
+ var logo=document.querySelector('#logo-name');
+ var food1=document.querySelector('#food-details1');
+ var food2=document.querySelector('#food-details2');
+ var food3=document.querySelector('#food-details3');
+ var food4=document.querySelector('#food-details4');
+ var food5=document.querySelector('#food-details5');
+ var food6=document.querySelector('#food-details6');
+ var x=document.querySelector('.contact-us p');
+ var y=document.querySelector('.foot1 p');
+ var z=document.querySelector('.foot2 p');
+ var k=document.querySelector('.foot3 p');
+ var footer=document.querySelector('#footer');
+ button.onclick=function (){  
+  
+      if(toggle.className=='open')  
+      {  
+          toggle.className="";  
+  
+  
+      }else{  
+          toggle.className="open";  
+  
+      }  
+  
+  
+      if(tog.className=='open')  
+      {  
+          tog.className="";  
+  
+  
+      }else{  
+          tog.className="open";  
+  
+      }  
+  
+      
+      if(header.className=='open')  
+      {  
+          header.className="";  
+  
+  
+      }else{  
+          header.className="open";  
+  
+      }  
+  
+  
+  
+      if(app.className=='open')  
+      {  
+          app.className="";  
+  
+  
+      }else{  
+          app.className="open";  
+  
+      }  
+      
+      
+      
+      if(body.className=='open')
+      {
+          body.className="";
+      }else{
+          body.className="open";
+      }
+      
+      if(btn.className=='open')
+      {
+          btn.className="";
+      }else{
+          btn.className="open";
+      }
+      
+      
+      if(logo.className=='open')
+      {
+          logo.className="";
+      }else{
+          logo.className="open";
+      }
+      
+      if(food1.className=='open')
+      {
+          food1.className="";
+      }else{
+          food1.className="open";
+      }
+      
+      if(food2.className=='open')
+      {
+          food2.className="";
+      }else{
+          food2.className="open";
+      }
+      if(food3.className=='open')
+      {
+          food3.className="";
+      }else{
+          food3.className="open";
+      }
+      if(food4.className=='open')
+      {
+          food4.className="";
+      }else{
+          food4.className="open";
+      }
+      if(food5.className=='open')
+      {
+          food5.className="";
+          
+      }else{
+          food5.className="open";
+      }
+      if(food6.className=='open')
+      {
+          food6.className="";
+      }else{
+          food6.className="open";
+      }
+      if(x.className=='open')
+      {
+          x.className="";
+      }else{
+          x.className="open";
+      }
+      if(y.className=='open')
+      {
+          y.className="";
+      }else{
+          y.className="open";
+      }
+      if(z.className=='open')
+      {
+          z.className="";
+      }else{
+          z.className="open";
+      }
+      if(k.className=='open')
+      {
+          k.className="";
+      }else{
+          k.className="open";
+      }
+      
+      
+      if(footer.className=='open')
+      {
+          footer.className="";
+      }else{
+          footer.className="open";
+      }
+      
+  };
